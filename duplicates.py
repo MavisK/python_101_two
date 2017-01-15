646f79525e61160c7d9cd9068082f4689ba2ea76
McLaren <artyudin@outlook.com>
2017-01-15 17:09:18 -0500

    Use empty files for exercises

--- duplicates.py
+++ duplicates.py
@@ -1,16 +1,4 @@
 def duplacates():
-	words_list = []
-
-	word = input("Enter a word (enter quit to quit): ")
-
-	while word != "quit":
-		if word not in words_list:
-			words_list.append(word)
-		word = input("Enter a word (enter quit to quit): ")
 	
-	if word == "quit":
-		for word in words_list:
-			print(word)
-
 
 duplacates()
